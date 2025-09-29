f065ba5cd2dbbc3801a2c786deed20f43edae083
Herval Mata <hervalmataamparo@gmail.com>
2025-09-29 15:34:14 -0300

    :sparkles: :feat crisnet - setting up sql server and create migrations
    Signed-off-by: Herval Mata <hervalmataamparo@gmail.com>

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+934c92771534042ce4e5145eba9e3c964c81f084")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f0912ccd420b13c8500d2ff888043eafbb9d408")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
